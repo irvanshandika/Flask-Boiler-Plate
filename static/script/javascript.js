c202220e982d4fd3492dd055f9304bfd0d6cae42
irvanshandika <shandikamuhammadirvan@gmail>
2024-07-16 12:18:06 +0700

    Flask Boilerplate

---
+++ static/script/javascript.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 20;
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+document.getElementById("js").innerHTML = sum(a, b);
+document.getElementById("js").innerHTML = sum(a, b);
